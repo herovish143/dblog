f6ae15e15004446c3a40ac5ab816d9044d6b50e4
Vishal Pal <vishalpal7994@gmail.com>
2021-03-19 16:16:56 +0530

    First commit by @herovish143

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+# Post model
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField('Category', related_name="posts")
+
+
+# Comment models
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
